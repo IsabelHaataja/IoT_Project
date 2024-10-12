628c1a9a8509322303a662241205c8f3abd45375
Isabel Haataja <isabelhaataja@gmail.com>
2024-10-12 22:48:39 +0200

    Styled the views

--- AC_Unit/Views/MainWindow.xaml.cs
+++ AC_Unit/Views/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using AC_Unit.ViewModels;
 using Iot_Recources.Data;
+using Iot_Recources.Services;
 using System.Windows;
 
 namespace AC_Unit.Views;
@@ -7,10 +8,14 @@ namespace AC_Unit.Views;
 public partial class MainWindow : Window
 {
     private readonly IDatabaseContext _context;
+    private readonly DeviceTwinManager _deviceTwinManager;
     public MainWindow(MainWindowViewModel viewModel, IDatabaseContext context)
     {
         InitializeComponent();
         DataContext = viewModel;
         _context = context;
+        //_deviceTwinManager = new DeviceTwinManager(_context);
+        //_deviceTwinManager.InitializeDeviceClientAsync().Wait();
+        //Task.Run(() => _deviceTwinManager.StartSendingDataAsync());
     }
 }
