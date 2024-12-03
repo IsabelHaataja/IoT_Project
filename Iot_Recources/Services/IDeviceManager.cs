6c3e9962a9e27495c759242af55d3596f34dba33
Isabel Haataja <isabelhaataja@gmail.com>
2024-12-03 19:06:32 +0100

    Updated interfaces according to methods in the services

--- Iot_Recources/Services/IDeviceManager.cs
+++ Iot_Recources/Services/IDeviceManager.cs
@@ -5,14 +5,12 @@ namespace Iot_Recources.Services;
 
 public interface IDeviceManager
 {
-    Task InitializeAsync();
-    Task PollDeviceTwinForChangesAsync();
-    DeviceClient? GetDeviceClient();
+    Task InitializeDeviceClientAsync();
+    Task PollDeviceTwinForChangesAsync(CancellationToken ct);
     Task<ResponseResult<string>> RegisterDeviceAsync(string deviceId);
     Task<ResponseResult> DisconnectAsync(CancellationToken ct);
     Task<ResponseResult<string>> SendDataAsync(string content, CancellationToken ct);
     Task<ResponseResult> ConnectToIotHubAsync(string deviceConnectionString);
     Task ReceiveCloudToDeviceMessagesAsync(CancellationToken ct);
-    Task SetUpDirectMethodHandlersAsync();
     event Action<string> OnDeviceStateChanged;
 }
--- Iot_Recources/Services/IDeviceTwinManager.cs
+++ Iot_Recources/Services/IDeviceTwinManager.cs
@@ -4,8 +4,8 @@ namespace Iot_Recources.Services
     public interface IDeviceTwinManager
     {
         Task InitializeAsync();
-        Task SendDataAsync(string content);
-        Task StartSendingDataAsync();
+        //Task SendDataAsync(string content);
+        //Task StartSendingDataAsync();
         Task UpdateDeviceTwinAsync(bool isDeviceOn);
     }
 }
